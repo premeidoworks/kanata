dbd8c99e92c6b0737e7acebf4627b8f143942c9f
goodplayer <woshiyigelaji@gmail.com>
2019-03-04 00:04:02 +0800

    initialize

---
+++ handler/funcs.go
@@ -0,0 +1,27 @@
+package handler
+
+import "net/http"
+
+func Publish(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func PrePublish(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Acquire(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Commit(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CommitPublish(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Bind(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/premeidoworks/kanata/handler"
+)
+
+var (
+	listenAddr string
+)
+
+func init() {
+	flag.StringVar(&listenAddr, "http", "0.0.0.0:8888", "-http=0.0.0.0:8888")
+}
+
+func main() {
+	mux := new(http.ServeMux)
+
+	mux.HandleFunc("/publish", handler.Publish)
+	mux.HandleFunc("/pre_publish", handler.PrePublish)
+	mux.HandleFunc("/acquire", handler.Publish)
+	mux.HandleFunc("/commit", handler.Publish)
+	mux.HandleFunc("/commit_publish", handler.CommitPublish)
+	mux.HandleFunc("/bind", handler.Publish)
+
+	err := http.ListenAndServe(listenAddr, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
